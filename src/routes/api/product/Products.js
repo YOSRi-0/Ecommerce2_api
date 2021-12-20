f81f9b0bea5ef407c412ae86dee32b7d5aa7d351
Yosri Hammadi <yosri.hammadi@gmail.com>
2021-12-20 13:55:46 +0100

    create products crud functionality

--- src/routes/api/product/Products.js
+++ src/routes/api/product/Products.js
@@ -1,3 +1,24 @@
+const { addColor } = require("../../../controllers/Colors");
+const {
+  createProduct,
+  getProduct,
+  getAllProducts,
+  deleteProduct,
+  deleteAllProducts,
+  updateProduct,
+} = require("../../../controllers/Products");
+
 const router = require("express").Router();
+
+router.post("/", createProduct);
+router.post("/color", addColor);
+
+router.get("/:id", getProduct);
+router.get("/", getAllProducts);
+
+router.delete("/:id", deleteProduct);
+router.delete("/", deleteAllProducts);
+
+router.put("/:id", updateProduct);
 
 module.exports = router;
